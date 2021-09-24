69cb7b4f995920b8823e4a25f298a362985e654f
Mateusz Drzał <mdrzal1886@gmail.com>
2021-09-24 20:20:51 +0200

    Create AppContext and put Provider to index.js

---
+++ src/AppContext.js
@@ -0,0 +1,93 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AppContext = createContext();
+
+const KeyAPPID = "92ed051ea808c3123500bd8d06fcb003";
+
+const AppProvider = ({ children }) => {
+  const [city, setCity] = useState("");
+  const [error, setError] = useState(false);
+  const [errorCode, setErrorCode] = useState(0);
+  const [weather, setWeather] = useState({
+    code: "",
+    description: "",
+    humidity: 0,
+    pressure: 0,
+    sunRise: 0,
+    sunSet: 0,
+    temperature: 0,
+    temperatureFeels: 0,
+    timezone: 0,
+    windSpeed: 0,
+  });
+
+  const getCityName = (city) => setCity(city);
+
+  useEffect(() => {
+    const APIurl = `http://api.openweathermap.org/data/2.5/weather?q=${city},&units=metric&APPID=${KeyAPPID}&lang=pl`;
+    if (city.length > 0) {
+      fetch(APIurl)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            setErrorCode(response.status);
+            return;
+          }
+        })
+        .then((data) => {
+          return setWeather({
+            code: data.weather[0].icon,
+            description: data.weather[0].description,
+            humidity: data.main.humidity,
+            pressure: data.main.pressure,
+            sunRise: data.sys.sunrise,
+            sunSet: data.sys.sunset,
+            temperature: data.main.temp,
+            temperatureFeels: data.main.feels_like,
+            timezone: data.timezone,
+            windSpeed: data.wind.speed,
+          });
+        })
+        .catch(() => {
+          setError(true);
+          return;
+        });
+    }
+  }, [city]);
+
+  const handleCloseClick = () => {
+    setWeather({
+      code: "",
+      description: "",
+      humidity: 0,
+      pressure: 0,
+      sunRise: 0,
+      sunSet: 0,
+      temperature: 0,
+      temperatureFeels: 0,
+      timezone: 0,
+      windSpeed: 0,
+    });
+    setError(false);
+    setCity("");
+    setErrorCode(0);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        city,
+        error,
+        errorCode,
+        getCityName,
+        handleCloseClick,
+        weather,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppProvider;
